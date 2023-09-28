342bcd73aac01a1215353d167d6c25df9b734b23
vincent <munenevincent49@gmail.com>
2023-09-28 21:19:57 +0300

    retrieve news

--- app/src/main/java/com/loc/newsapp/ft_news_navigation/data/remote/NewsApi.kt
+++ app/src/main/java/com/loc/newsapp/ft_news_navigation/data/remote/NewsApi.kt
@@ -2,16 +2,15 @@ package com.loc.newsapp.ft_news_navigation.data.remote
 
 import com.loc.newsapp.ft_news_navigation.data.remote.dto.NewsResponse
 import com.loc.newsapp.util.constants.BASEURL
+import com.loc.newsapp.util.constants.TOKEN
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface NewsApi {
     @GET("everything")
-
     suspend fun getNews(
-        @Header("Authorization") token: String,
-        @Query("page") page: Int,
-        @Query("source") source:String
+        @Header("Authorization") token:String,
+        @Query("sources") sources: String
     ): NewsResponse
 }
